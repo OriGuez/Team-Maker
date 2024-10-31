574982a3f984612f50296d90e3a2527c2824570d
OriGuez <orig200@gmail.com>
2024-10-31 20:50:23 +0200

    update2

--- Team Maker/TeamMaker.js
+++ Team Maker/TeamMaker.js
@@ -154,28 +154,7 @@ function addSelectedPlayers() {
     }
   
   }
-  function resetCheckboxes() {
-    var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
-    var playersSheet = spreadsheet.getSheetByName("Players"); // Change "Players" to the name of your players tab
-    
-    // Clear all checkboxes in the "Players" tab
-    var playersRange = playersSheet.getRange("A2:A");
-    playersRange.uncheck();
-  }
   
-  function increment() {
-    var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
-    var cell = sheet.getRange('F2'); 
-    var value = cell.getValue();
-    cell.setValue(value + 0.1);
-  }
-  
-  function decrement() {
-    var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
-    var cell = sheet.getRange('F2'); 
-    var value = cell.getValue();
-    cell.setValue(value - 0.1);
-  }
   
   
   
